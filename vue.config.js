d0d3e2643413e05b5475d194879bc6bb277cd02b
Iliyan <djrfung@outlook.com>
2020-08-14 22:18:44 -0700

    Fix broken refs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ?
-    '/PFW-UI-Mockup/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ?
+  //   '/PFW-UI-Mockup/' : '/',
+  publicPath: '/PFW-UI-Mockup/',
   outputDir: 'docs'
 }
